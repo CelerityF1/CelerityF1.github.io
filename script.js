d3f32449e1af0794c9922a91f390116cae61c7fe
rain-1107 <83094739+rain-1107@users.noreply.github.com>
2023-06-08 17:41:46 +0100

    0.1.2

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
-const canvas = document.getElementById('mainCanvas')
-const c = canvas.getContext('2d')
-const sf = 0.5
-const d = new Date()
-let objects = []
+const canvas = document.getElementById("mainCanvas");
+const c = canvas.getContext("2d");
+const sf = 0.5;
+let objects = [];
 
 function pixel(num) {
-  return num * (canvas.width / 1000)
+  return num * (canvas.width / 1000);
 }
 
 class Grid {
   constructor() {
-    this.moles = []
+    this.moles = [];
   }
   createGrid() {
-    let size = 100
-    let padding = 50
-    let y_height = 200
+    let size = 100;
+    let padding = 50;
+    let y_height = 200;
     for (var x = -1; x < 2; x++) {
       for (var y = -1; y < 2; y++) {
-        this.moles.push(new Mole({ x: pixel(450 + (x * (size + padding))), y: pixel(y_height + (y * (size + padding))) }, { x: pixel(size), y: pixel(size) }))
+        this.moles.push(
+          new Mole({
+            x: pixel(450 + x * (size + padding)),
+            y: pixel(y_height + y * (size + padding)),
+          }, { x: pixel(size), y: pixel(size) })
+        );
       }
     }
   }
   onClick(position) {
     for (var i = 0, l = this.moles.length; i < l; i++) {
-      if (this.moles[i].sprite.position.x < position.x && position.x < this.moles[i].sprite.position.x + this.moles[i].sprite.size.x) {
-        if (this.moles[i].sprite.position.y < position.y && position.y < this.moles[i].sprite.position.y + this.moles[i].sprite.size.y) {
-          this.moles[i].onClick()
-          console.log(position)
+      if (
+        this.moles[i].sprite.position.x < position.x &&
+        position.x <
+        this.moles[i].sprite.position.x + this.moles[i].sprite.size.x
+      ) {
+        if (
+          this.moles[i].sprite.position.y < position.y &&
+          position.y <
+          this.moles[i].sprite.position.y + this.moles[i].sprite.size.y
+        ) {
+          this.moles[i].onClick();
         }
       }
     }
@@ -35,107 +46,104 @@ class Grid {
   update() {
     for (var i = 0, l = this.moles.length; i < l; i++) {
       // console.log(this.mole[i].position)
-      this.moles[i].update()
+      this.moles[i].update();
     }
   }
 }
 
-const grid = new Grid()
+const grid = new Grid();
 
 class Mole {
   constructor(position, size) {
-    this.sprite = new Sprite(position, size)
-    this.active = 0
-    this.timeLeft = -1
-    this.clicked_on = false
-    this.lastCall = d.getTime()
-    this.current = d.getTime()
+    this.sprite = new Sprite(position, size);
+    this.active = 0;
+    this.timeLeft = -1;
+    this.clicked_on = false;
+    this.lastCall = Date.now() / 1000;
+    this.current = Date.now() / 1000;
   }
   activate(duration) {
-    this.active = 1
-    this.timeLeft = duration
+    this.active = 1;
+    this.timeLeft = duration;
   }
   onClick() {
-    console.log(this)
-    this.clicked_on = true
+    this.clicked_on = true;
   }
   update() {
+    this.current = Date.now() / 1000;
     switch (this.active) {
       case 0:
-        this.sprite.draw("black")
+        this.sprite.draw("black");
         if (this.clicked_on) {
-          this.active = 2
-          this.timeLeft = 1
-          this.clicked_on = false
+          this.active = 2;
+          this.timeLeft = 1;
+          this.clicked_on = false;
         }
         break;
       case 1:
         if (this.clicked_on) {
-          this.active = 0
-          this.timeLeft = -1
-          this.clicked_on = false
-          this.sprite.draw("black")
-          break
+          this.active = 0;
+          this.timeLeft = -1;
+          this.clicked_on = false;
+          this.sprite.draw("black");
+          break;
         }
-        this.current = d.getTime()
-        this.sprite.draw("green")
-        this.timeLeft = this.timeLeft - (this.current - this.lastCall)
+        this.sprite.draw("red");
+        this.timeLeft = this.timeLeft - (this.current - this.lastCall);
         if (this.timeLeft <= 0) {
-          this.active = 0
-          this.timeLeft = -1
+          this.active = 0;
+          this.timeLeft = -1;
         }
-        this.lastCall = this.current
         break;
 
       case 2:
-        this.current = d.getTime()
-        this.sprite.draw("green")
-        this.timeLeft = this.timeLeft - (this.current - this.lastCall)
+        this.sprite.draw("black");
+        this.timeLeft = this.timeLeft - (this.current - this.lastCall);
+        console.log(this.current - this.lastCall);
         if (this.timeLeft <= 0) {
-          this.active = 1
-          this.timeLeft = 3
+          this.active = 1;
+          this.timeLeft = 3;
         }
-        this.lastCall = this.current
-        break
+        break;
     }
-
+    this.lastCall = this.current
   }
 }
 
 class Sprite {
   constructor(position, size) {
-    this.position = position
-    this.size = size
-    this.sf = sf
-    return this
+    this.position = position;
+    this.size = size;
+    this.sf = sf;
+    return this;
   }
   draw(colour) {
-    c.fillStyle = colour
-    c.fillRect(this.position.x, this.position.y, this.size.x, this.size.y)
-    c.fillStyle = "blue"
+    c.fillStyle = colour;
+    c.fillRect(this.position.x, this.position.y, this.size.x, this.size.y);
+    c.fillStyle = "blue";
   }
 }
 
 function loop() {
-  window.requestAnimationFrame(loop)
-  c.fillStyle = "grey"
-  c.fillRect(0, 0, canvas.width, canvas.height)
-  grid.update()
+  window.requestAnimationFrame(loop);
+  c.fillStyle = "grey";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  grid.update();
 }
 
 function click(event) {
   //console.log("stuff")
-  grid.onClick({ x: event.clientX, y: event.clientY })
+  grid.onClick({ x: event.clientX, y: event.clientY });
 }
 
-document.addEventListener("click", click)
+document.addEventListener("click", click);
 
 function init() {
-  let button = document.getElementById('startButton')
-  button.hidden = true
+  let button = document.getElementById("startButton");
+  button.hidden = true;
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  c.fillRect(0, 0, canvas.width, canvas.height)
-  grid.createGrid()
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  grid.createGrid();
   loop();
 }
